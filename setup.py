258a4765eb0b9a9ed8996424fabf5cb8a7ee10c7
Jerome Belleman <jerome.belleman@gmail.com>
2016-11-15 15:04:51 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
+# coding=utf-8
 
+import os
 from distutils.core import setup
 
-setup(name='tasks',
-      scripts=['tasks'],
-      data_files=[
-                  ('share/tasks', ['vimrc', 'template']),
-                 ],
-     )
+setup(
+    name='tasks',
+    version='1.0',
+    author='Jérôme Belleman',
+    author_email='Jerome.Belleman@gmail.com',
+    url='http://cern.ch/jbl',
+    description='"Manage tasks"',
+    long_description='"Manage tasks in projects, according to priority, using Vim."',
+    scripts=['tasks'],
+    data_files=[('share/tasks', ['vimrc', 'template']), ('share/man/man1', ['tasks.1'])],
+)
